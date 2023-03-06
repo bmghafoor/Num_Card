ab6a1f27b59f1d463b869cf13c164f3b343dd73c
Binyameen <bmghafoor@gmail.com>
2023-03-05 21:07:44 -0500

    part2

--- cards.js
+++ cards.js
@@ -1,22 +1,22 @@
-axios.get(`https://deckofcardsapi.com/api/deck/new/draw/`).then(resp => {
-    console.log(`${resp.data['cards'][0]['value']} of ${resp.data['cards'][0]['suit']}` )
-});
+// axios.get(`https://deckofcardsapi.com/api/deck/new/draw/`).then(resp => {
+//     console.log(`${resp.data['cards'][0]['value']} of ${resp.data['cards'][0]['suit']}` )
+// });
 
-function getTwoCards(){
-    axios.get(`https://deckofcardsapi.com/api/deck/new/draw/`).then(resp => {
-    firstCard = `${resp.data['cards'][0]['value']} of ${resp.data['cards'][0]['suit']}`
-    newUrl = `https://deckofcardsapi.com/api/deck/${resp.data['deck_id']}/draw/`
-    axios.get(newUrl).then(resp => {
-        secondCard = `${resp.data['cards'][0]['value']} of ${resp.data['cards'][0]['suit']}`
-        console.log(firstCard,secondCard)
-    })
-});
-}
+// function getTwoCards(){
+//     axios.get(`https://deckofcardsapi.com/api/deck/new/draw/`).then(resp => {
+//     firstCard = `${resp.data['cards'][0]['value']} of ${resp.data['cards'][0]['suit']}`
+//     newUrl = `https://deckofcardsapi.com/api/deck/${resp.data['deck_id']}/draw/`
+//     axios.get(newUrl).then(resp => {
+//         secondCard = `${resp.data['cards'][0]['value']} of ${resp.data['cards'][0]['suit']}`
+//         console.log(firstCard,secondCard)
+//     })
+// });
+// }
 
-let deckId = null;
-let $btn = $('button');
-let $cardArea = $('#card-area');
-let baseURL = 'https://deckofcardsapi.com/api/deck';
+// let deckId = null;
+// let $btn = $('button');
+// let $cardArea = $('#card-area');
+// let baseURL = 'https://deckofcardsapi.com/api/deck';
 
 $(function() {
     let baseURL = 'https://deckofcardsapi.com/api/deck';
@@ -43,3 +43,39 @@ $(function() {
     });
   });
   
+// Part 2
+
+async function getOneCard(){
+  res = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/`)
+  console.log(`${res.data['cards'][0]['value']} of ${res.data['cards'][0]['suit']}` )
+}
+
+async function getTwoCards(){
+  res = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/`)
+  firstCard = `${res.data['cards'][0]['value']} of ${res.data['cards'][0]['suit']}`
+  newUrl = `https://deckofcardsapi.com/api/deck/${res.data['deck_id']}/draw/`
+
+  res2 = await axios.get(newUrl)
+  secondCard = `${res2.data['cards'][0]['value']} of ${res2.data['cards'][0]['suit']}`
+  console.log(firstCard,secondCard)
+}
+
+
+async function clickpage(){
+  let baseURL = 'https://deckofcardsapi.com/api/deck';
+    
+  let deckId = null;
+  let $btn = $('button');
+  let $cardArea = $('#card-area');
+
+  res = await axios.get(`${baseURL}/new/shuffle/`)
+  deckId = res.deck_id;
+  $btn.show();
+
+  $btn.on('click'), async function() {
+    res = await axios.get(`${baseURL}/${deckId}/draw/`)
+    let cardSrc = res.cards[0].image;
+    $cardArea.append($('<img>', {src: cardSrc}));
+    if (data.remaining === 0) $btn.remove();
+  }
+}
--- num.js
+++ num.js
@@ -4,17 +4,30 @@
 
 
 
-function funfact(num) {
-    for(let i = 0; i < 4 ;i++){
-        let li = document.createElement("li");
-        axios.get(`http://numbersapi.com/${num}/`).then(resp => {
-            li.innerHTML = resp.data
-            console.log(li)
-            $('#array').append(li)
-        });
+// function funfact(num) {
+//     for(let i = 0; i < 4 ;i++){
+//         let li = document.createElement("li");
+//         axios.get(`http://numbersapi.com/${num}/`).then(resp => {
+//             li.innerHTML = resp.data
+//             console.log(li)
+//             $('#array').append(li)
+//         });
 
+//     }
+// }
+
+// funfact('23')
+
+// Part 2 async await
+
+async function getFunFacts(num) {
+    for(let i = 0; i < 4; i++){
+        let li = document.createElement('li')
+        res = await axios.get(`http://numbersapi.com/${num}/`)
+        li.innerHTML = res.data
+        console.log(li)
+        $('#array').append(li)
     }
 }
 
-funfact('23')
 
